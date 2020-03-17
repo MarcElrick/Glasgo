8378e2cde74214794872dd7f966e558e4f7078a7
Marc Elrick <marcelrick@pop-os.localdomain>
2020-03-17 15:35:50 +0000

    pushing changes to new branch to sort potential merge conflict

--- app/src/main/java/com/example/glasgo/MapsActivity.java
+++ app/src/main/java/com/example/glasgo/MapsActivity.java
@@ -31,7 +31,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng mainBuilding = new LatLng(55.872332, -4.288034);
-        mMap.addMarker(new MarkerOptions().position(mainBuilding).title("Marker in Glasgow"));
+        mMap.addMarker(new MarkerOptions().position(mainBuilding).title("You starting here!"));
 
         float zoomLevel = 16.0f;
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mainBuilding, zoomLevel));
