ddd5c7b5d33f3af886c172878461fa9d8a3108a7
Marc Elrick <marcelrick@pop-os.localdomain>
2020-03-23 15:24:34 +0000

    Added button to go back to maps activity.

--- app/src/main/java/com/example/glasgo/BuildingInfoActivity.java
+++ app/src/main/java/com/example/glasgo/BuildingInfoActivity.java
@@ -2,7 +2,9 @@ package com.example.glasgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BuildingInfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class BuildingInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building_info);
+    }
+
+    public void startMapActivityAuth(View view){
+        Intent mapPageIntent = new Intent(this, MapsActivity.class);
+        mapPageIntent.putExtra("authenticated", true);
+        startActivity(mapPageIntent);
     }
 }
